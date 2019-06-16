f7fa144fb16b8598ec96e66ad128b43ab42b8753
Mr Ezak <ezaksaeed@gmail.com>
2019-06-16 17:05:05 +0430

    Create login page

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Login = () => (
+  <div>
+    <LoginBox />
+  </div>
+)
+
+export default Login
+
+const LoginBox = styled.div`
+  width: 300px;
+  height: 300px;
+  background: #ccc;
+  margin: 200px auto;
+`
